5720350ba8e0a0827f22e02f89ebb04d3a87f0db
MaradiSu <p.suresh@campusuvce.in>
2020-10-17 16:45:30 +0530

    operators commit

--- c02/js/add-content.js
+++ c02/js/add-content.js
@@ -4,9 +4,9 @@
 function ExecFunc()
 {
 	//Your Code Goes Here
-	alert("Hi");
-	
-	console.error("error is there");
+	var foo = 'Hello';
+var bar = 'world';
+console.log(foo + bar); // ’ h e l l o world ’
 	
 	
 	
