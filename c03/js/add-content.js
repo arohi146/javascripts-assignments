68f043cec37c4c30246671ea1a1826665f0a2c3d
Poojraokale <poojaraokale@gmail.com>
2020-10-30 02:12:14 -0700

    updated switch statmt

--- c03/js/add-content.js
+++ c03/js/add-content.js
@@ -1,13 +1,38 @@
 
 
 
-function ExecFunc()
+function conditionalcode()
 {
-	//Your Code Goes Here
-	alert("Hi");
-	
-	console.error("error is there");
-	
-	
-	
+	var foo = true ;
+	var bar = false ;
+	if ( bar ) {
+	// this code will never run
+	console.log ( 'hello !');
+	}
+	if ( bar ) {
+	// this code won’t run
+	} else {
+	if ( foo ) {
+	// this code will run
+	} else {
+	// this code would run if foo and bar were both false
+	}
+}
+	console.log(foo);
+}
+function switch_statement()
+{
+	var foo=true;
+	switch ( foo ) {
+		case 'bar':
+		alert ( 'the value was bar -- yay ! ');
+		break ;
+		case 'baz':
+		alert ( 'boo baz :( ');
+		break ;
+		default :
+		alert ( 'everything else is just ok ');
+		break ;
+		}
+		console.log(baz);
 }
