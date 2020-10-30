f2ee6ee5e55288feef19e714eabe6b67cb7d5923
Poojraokale <poojaraokale@gmail.com>
2020-10-30 01:08:56 -0700

    updated comparison operators

--- c02/js/add-content.js
+++ c02/js/add-content.js
@@ -46,5 +46,18 @@ function logicaloperators()
 }
 function comparisonoperators()
 {
-	
+	var foo = 1;
+	var bar = 0;
+	var baz = '1';
+	var bim = 2;
+	foo == bar ; //  returns false
+	foo != bar ; //  returns true
+	foo == baz ; //  returns true ; careful !
+	foo === baz ; //  returns false
+	foo !== baz ; //  returns true
+	foo === parseInt( baz ); // returns true
+	foo > bim ; // returns false
+	bim > baz ; // returns true
+	foo <= baz ; // returns true
+	console.log(foo==baz);
 }
