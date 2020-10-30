7325b91a63d5a1e613bff6147dabaa3cf1bcec64
Poojraokale <poojaraokale@gmail.com>
2020-10-30 06:33:28 -0700

    updated simple functions

--- c07/js/add-content.js
+++ c07/js/add-content.js
@@ -1,13 +1,42 @@
 
 
 
-function ExecFunc()
+function simplefunctions()
 {
-	//Your Code Goes Here
-	alert("Hi");
+	var greet = function ( person , greeting ) {
+		var text = greeting +  ' , ' + person ;
+		console. log ( text );
+		};
+		greet ( ' Rebecca ' , ' Hello ');
 	
-	console.error("error is there");
 	
-	
-	
+}
+function returnvalue()
+{
+	var greet = function ( person , greeting ) {
+		var text = greeting +  ',' + person ;
+		return text ;
+		};
+		console.log ( greet ( ' Rebecca ' , ' hello '));
+}
+function returnfunctions()
+{
+	var greet = function ( person , greeting ) {
+		var text = greeting + ' , ' + person ;
+		return function() { console.log( text ); };
+		};
+		var greeting = greet ( 'Rebecca ' , ' Hello ');
+		greeting ();
+		
+}
+function passingargumnents()
+{
+	var myFn = function ( fn ) {
+		var result = fn ();
+		console.log ( result );
+		};
+		var myOtherFn = function() {
+		return ' hello world ';
+		};
+		myFn ( myOtherFn ); // logs ' h e l l o world '
 }
